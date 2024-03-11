22d49c36e72b36b7cf6dcc26c624ff0afbbc34c6
AmethystMovie14 <129354257+AmethystMovie14@users.noreply.github.com>
2024-03-10 22:47:07 -0600

    Pagina web
    Aun en testeo

--- index.py
+++ index.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Test'
+    return render_template('home.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-if __name___ == '__main__':
-    app.run()
+@app.route('/test')
+def test():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
